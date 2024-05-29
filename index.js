f10a0df2e306f2d8f8d2877a593e3f83560099e9
Sabid Farhan <sfttanim@gmail.com>
2024-05-29 22:13:02 +0600

    added firebase code and some for get

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+require('dotenv').config();
+
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json());
+
+// MongoDB Start
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9sxzsr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const allCategoryJob = client.db("assignment-011").collection('allJobs')
+
+        app.get('/allCategoryJob', async (req, res) => {
+            const cursor = allCategoryJob.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/allCategoryJob/:category', async (req, res) => {
+            const params = req.params.category;
+            const query = { category: params };
+            const cursor = allCategoryJob.find(query);
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+// MongoDB Finish
+
+
 
 
 
