9a3442a03a74a2707c30e7ddc1e2acf65e654451
Sabid Farhan <sfttanim@gmail.com>
2024-05-30 01:33:50 +0600

    all done for home page

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // MongoDB Start
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9sxzsr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,6 +48,13 @@ async function run() {
             const cursor = allCategoryJob.find(query);
             const result = await cursor.toArray()
             res.send(result)
+        })
+
+        app.get('/allCategory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await allCategoryJob.findOne(query);
+            res.send(result);
         })
     } finally {
         // Ensures that the client will close when you finish/error
